a528647a89a405a10d240b5444798d181e0aa04d
harry388 <harrythompson3765@gmail.com>
2024-03-25 15:46:58 +0000

    steam

--- configuration.nix
+++ configuration.nix
@@ -125,6 +125,12 @@
   #   enableSSHSupport = true;
   # };
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
   programs.nixvim = {
     enable = true;
 
