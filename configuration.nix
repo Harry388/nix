4adfe84f9066afa7d751b51151465fc542618007
harry388 <harrythompson3765@gmail.com>
2024-04-11 20:00:07 +0100

    nixvim harpoon

--- configuration.nix
+++ configuration.nix
@@ -183,7 +183,19 @@
       treesitter.enable = true;
       telescope.enable = true;
       undotree.enable = true;
-      harpoon.enable = true;
+      harpoon = {
+          enable = true;
+          keymaps = {
+              toggleQuickMenu = "<C-e>";
+              addFile = "<leader>a";
+              navFile = {
+                "1" = "<leader>h";
+                "2" = "<leader>j";
+                "3" = "<leader>k";
+                "4" = "<leader>l";
+              };
+          };
+      };
       oil = {
           enable = true;
           settings.columns = [ "icon" ];
@@ -213,8 +225,8 @@
           gt = "type_definition";
         };
         keymaps.diagnostic = {
-          "<leader>j" = "goto_next";
-          "<leader>k" = "goto_prev";
+          "<leader>n" = "goto_next";
+          "<leader>p" = "goto_prev";
         };
       };
       cmp = {
@@ -254,17 +266,6 @@
     ];
 
     extraConfigLua = ''
-      local mark = require("harpoon.mark")
-      local ui = require("harpoon.ui")
-
-      vim.keymap.set("n", "<leader>a", mark.add_file)
-      vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-
-      -- vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-      -- vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
-      -- vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
-      -- vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
-
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
       vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
