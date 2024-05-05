07b629f1b9a8a6c3319902c55931f9455b2fb4f1
harry388 <harrythompson3765@gmail.com>
2024-05-05 16:17:55 +0100

    195 gnome update

--- modules/desktops/gnome.nix
+++ modules/desktops/gnome.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 
 let 
     cfg = config.gnome;
@@ -22,6 +22,29 @@ in
             layout = "gb";
             variant = "";
         };
+
+        environment.gnome.excludePackages = (with pkgs; [
+            gnome-photos
+            gnome-tour
+            gedit
+        ]) ++ (with pkgs.gnome; [
+            cheese # webcam tool
+            gnome-music
+            epiphany # web browser
+            geary # email reader
+            gnome-characters
+            tali # poker game
+            iagno # go game
+            hitori # sudoku game
+            atomix # puzzle game
+            yelp # Help view
+            gnome-contacts
+            gnome-initial-setup
+        ]);
+        programs.dconf.enable = true;
+        environment.systemPackages = with pkgs; [
+            gnome.gnome-tweaks
+        ];
     };
 
 }
