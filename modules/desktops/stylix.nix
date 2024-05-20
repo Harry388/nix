81c3fb2b2ef1edc9e1e903f0a89c86ef249492ea
harry388 <harrythompson3765@gmail.com>
2024-05-20 15:38:59 +0100

    289 cursor

--- modules/desktops/stylix.nix
+++ modules/desktops/stylix.nix
@@ -17,6 +17,11 @@ in
 
         stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
 
+        stylix.cursor = {
+            package = pkgs.bibata-cursors;
+            name = "Bibata-Modern-Ice";
+        };
+
     };
 
 }
