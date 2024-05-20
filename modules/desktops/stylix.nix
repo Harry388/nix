530a8b21458846d18660f7ee571d68c24c4171f1
harry388 <harrythompson3765@gmail.com>
2024-05-20 15:55:29 +0100

    291 cursor

--- modules/desktops/stylix.nix
+++ modules/desktops/stylix.nix
@@ -18,8 +18,8 @@ in
         stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
 
         stylix.cursor = {
-            package = pkgs.bibata-cursors;
-            name = "Bibata-Modern-Ice";
+            package = pkgs.apple-cursor;
+            name = "macOS-BigSur-White";
         };
 
         stylix.fonts = {
