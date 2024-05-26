7ec61ed61a0645c011ad13db05c07bf4e5e921eb
harry388 <harrythompson3765@gmail.com>
2024-05-26 14:21:25 +0100

    334 everforest

--- modules/desktops/stylix.nix
+++ modules/desktops/stylix.nix
@@ -15,7 +15,7 @@ in
 
         stylix.polarity = "dark";
 
-        stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+        stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest-dark-hard.yaml";
 
         stylix.cursor = {
             package = pkgs.apple-cursor;
