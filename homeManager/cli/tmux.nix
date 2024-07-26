90169a7c0190f3b7191adcae9941fc81d9aad12e
Harry <harrythompson3765@gmail.com>
2024-07-26 09:46:13 +0100

    16 tmux nav

--- homeManager/cli/tmux.nix
+++ homeManager/cli/tmux.nix
@@ -23,8 +23,8 @@ in
             escapeTime = 0;
 
             extraConfig = ''
-                bind -n C-] next-window
-                bind -n C-[ previous-window
+                bind -n C-M-L next-window
+                bind -n C-M-H previous-window
                 bind -T copy-mode-vi v send -X begin-selection
                 bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel
                 bind -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel
