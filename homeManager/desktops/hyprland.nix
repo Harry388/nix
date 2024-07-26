f1e85c8b12a64a298de196db95e302eef209463b
Harry <harrythompson3765@gmail.com>
2024-07-26 13:25:46 +0100

    44

--- homeManager/desktops/hyprland.nix
+++ homeManager/desktops/hyprland.nix
@@ -184,8 +184,8 @@ in
             enable = true;
             settings = {
                 monitor = [
-                    "DP-1, 1920x1200, 0x0, 1"
-                    "DP-2, 1920x1080, 1920x0, 1"
+                    "eDP-1, 1920x1200, 0x0, 1"
+                    "HDMI-A-1, 1920x1080, 1920x0, 1"
                 ];
 
                 "$terminal" = "alacritty";
