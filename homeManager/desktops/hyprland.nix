63363f05f6eee70968e9073ecac31947213a7908
Harry <harrythompson3765@gmail.com>
2024-07-26 13:38:57 +0100

    47

--- homeManager/desktops/hyprland.nix
+++ homeManager/desktops/hyprland.nix
@@ -83,6 +83,18 @@ in
                     "$mainMod, g, togglefloating"
                 ];
 
+                bindel = [
+                    ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+                    ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+                ];
+
+                bindl = [
+                    ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+                    ", XF86AudioPlay, exec, playerctl play-pause"
+                    ", XF86AudioPrev, exec, playerctl previous"
+                    ", XF86AudioNext, exec, playerctl next"
+                ];
+
             };
         };
 
@@ -115,6 +127,7 @@ in
         home.packages = with pkgs; [
             fuzzel
             waybar
+            playerctl
         ];
 
     };
