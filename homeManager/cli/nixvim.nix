a9337a2cc2ceee72f203ac585c74d71c768decb5
Harry Thompson <harrythompson3765@gmail.com>
2024-08-18 19:30:41 +0100

    64

--- homeManager/cli/nixvim.nix
+++ homeManager/cli/nixvim.nix
@@ -211,6 +211,7 @@ in
                 rainbow-delimiters = {
                     enable = true;
                     highlight = [ "RainbowDelimiterYellow" "RainbowDelimiterBlue" "RainbowDelimiterOrange" "RainbowDelimiterGreen" "RainbowDelimiterViolet" "RainbowDelimiterCyan" ];
+                    blacklist = [ "zig" ];
                 };
                 tmux-navigator = {
                     enable = true;
