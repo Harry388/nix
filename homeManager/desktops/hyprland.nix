79d847e76936f7e717a6f08166871be51f2e63af
Harry Thompson <harrythompson3765@gmail.com>
2024-08-26 21:51:51 +0100

    87 waybar

--- homeManager/desktops/hyprland.nix
+++ homeManager/desktops/hyprland.nix
@@ -224,7 +224,7 @@ window#waybar {
 }
 
 #workspaces {
-    background-color: rgba(0,0,0,0.4);
+    background-color: rgba(0,0,0,0.6);
     border: 2px solid #8ec07c;
 }
 
@@ -244,7 +244,7 @@ window#waybar {
 }
 
 .modules-center {
-    background-color: rgba(0,0,0,0.4);
+    background-color: rgba(0,0,0,0.6);
 }
 
 #battery {
@@ -254,13 +254,13 @@ window#waybar {
     margin-left: 15px;
     color: #83a598;
     border: 2px solid #83a598;
-    background-color: rgba(0,0,0,0.4);
+    background-color: rgba(0,0,0,0.6);
 }
 
 #clock {
     padding-right: 20px;
     padding-left: 20px;
-    background-color: rgba(0,0,0,0.4);
+    background-color: rgba(0,0,0,0.6);
     color: #b8bb26;
     border: 2px solid #b8bb26;
 }
@@ -268,7 +268,7 @@ window#waybar {
 #tray {
     padding-right: 20px;
     padding-left: 20px;
-    background-color: rgba(0,0,0,0.4);
+    background-color: rgba(0,0,0,0.6);
     color: #ffffff;
     border: 2px solid #ffffff;
 }
@@ -276,7 +276,7 @@ window#waybar {
 #network {
     padding-right: 20px;
     padding-left: 20px;
-    background-color: rgba(0,0,0,0.4);
+    background-color: rgba(0,0,0,0.6);
     color: #d3869b;
     border: 2px solid #d3869b;
 }
@@ -286,7 +286,7 @@ window#waybar {
     padding-left: 20px;
     margin-left: 15px;
     margin-right: 15px;
-    background-color: rgba(0,0,0,0.4);
+    background-color: rgba(0,0,0,0.6);
     color: #fabd2f;
     border: 2px solid #fabd2f;
 }
@@ -294,7 +294,7 @@ window#waybar {
 #backlight {
     padding-right: 20px;
     padding-left: 20px;
-    background-color: rgba(0,0,0,0.4);
+    background-color: rgba(0,0,0,0.6);
     color: #fe8019;
     border: 2px solid #fe8019;
 }
