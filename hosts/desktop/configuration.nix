c136239302da432b4a0a588362ded8fe5295c20b
Harry Thompson <harrythompson3765@gmail.com>
2024-09-06 17:44:41 +0100

    124 cachix

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -131,6 +131,11 @@
 
   nvidia.enable = true;
 
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
