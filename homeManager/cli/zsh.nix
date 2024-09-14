a34ab1c7120806cf985c64a81233a5d9530afa7c
Harry Thompson <harrythompson3765@gmail.com>
2024-09-14 20:00:45 +0100

    130 rm update

--- homeManager/cli/zsh.nix
+++ homeManager/cli/zsh.nix
@@ -32,7 +32,6 @@ in
             enableCompletion = true;
 
             shellAliases = {
-                update = "nix flake update";
                 krisp = "nix run \"github:steinerkelvin/dotfiles#discord-krisp-patch\"";
                 generation = "sudo nix-env --list-generations --profile /nix/var/nix/profiles/system | grep current | awk '{print $1}'";
                 sail = "sh $([ -f sail ] && echo sail || echo vendor/bin/sail)";
