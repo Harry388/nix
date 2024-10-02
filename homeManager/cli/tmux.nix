8b7ba19d765f86f951f25f03d97128b3c7263784
Harry Thompson <harrythompson3765@gmail.com>
2024-10-02 21:43:15 +0100

    157 tmux

--- homeManager/cli/tmux.nix
+++ homeManager/cli/tmux.nix
@@ -18,8 +18,6 @@
 
         extraConfig = ''
             set -ag terminal-overrides ",alacritty:RGB"
-            bind L next-window
-            bind H previous-window
             bind -T copy-mode-vi v send -X begin-selection
             bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel
             bind -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel
