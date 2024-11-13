b2b7c99ed55bc156a3917f2e667184bbcff04311
Harry Thompson <harrythompson3765@gmail.com>
2024-11-13 17:15:55 +0000

    250 denols

--- homeManager/cli/nixvim.nix
+++ homeManager/cli/nixvim.nix
@@ -142,23 +142,17 @@
                 servers = {
                     denols = {
                         enable = true;
+                        settings = {
+                            root_dir = "lspconfig.util.root_pattern(\"deno.json\")";
+                        };
                         onAttach.function = ''
-                            local active_clients = vim.lsp.get_active_clients()
-                            if client.name == 'denols' then
-                                for _, client_ in pairs(active_clients) do
-                                    -- stop tsserver if denols is already active
-                                    if client_.name == 'tsserver' then
-                                        client_.stop()
-                                    end
-                                end
-                            elseif client.name == 'tsserver' then
-                                for _, client_ in pairs(active_clients) do
-                                    -- prevent tsserver from starting if denols is already active
-                                    if client_.name == 'denols' then
-                                        client.stop()
-                                    end
-                                end
+                        local active_clients = vim.lsp.get_active_clients()
+                        for _, client in pairs(active_clients) do
+                            -- stop tsserver if denols is already active
+                            if client.name == "tsserver" then
+                                client.stop()
                             end
+                        end
                         '';
                     };
                     ts_ls.enable = true;
