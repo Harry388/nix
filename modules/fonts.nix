921f57e5e647aab202044e27b1092955a538948b
Harry Thompson <harrythompson3765@gmail.com>
2024-11-30 23:34:32 +0000

    108 update

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -6,7 +6,7 @@
   fonts = {
       enableDefaultPackages = true;
       packages = with pkgs; [
-          (nerdfonts.override { fonts = ["JetBrainsMono"]; })
+          nerd-fonts.jetbrains-mono
           noto-fonts
       ];
       fontconfig = {
