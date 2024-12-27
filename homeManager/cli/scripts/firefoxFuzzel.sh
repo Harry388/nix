aa9d6c8a59c8b7ce326a3d0fc826b5f6eb1c58a6
Harry Thompson <harrythompson3765@gmail.com>
2024-12-27 17:10:22 +0000

    332 profile

--- homeManager/cli/scripts/firefoxFuzzel.sh
+++ homeManager/cli/scripts/firefoxFuzzel.sh
@@ -1,4 +1,4 @@
-profile=$(cat ~/.mozilla/firefox/profiles.ini | grep Name= | awk -F= '{print $2}' | fuzzel --placeholder="Firefox Pofile" --background-color=000000FF --border-color=E66000FF --border-width=2 --text-color=FFFFFFFF --selection-color=331E54FF --selection-text-color=FFFFFFFF --prompt-color=FFFFFFFF --selection-match-color=E66000FF --dmenu)
-if [[ -n $profile ]] then
-    firefox -P $profile
+profile=$(grep Name= < ~/.mozilla/firefox/profiles.ini  | awk -F= '{print $2}' | fuzzel --placeholder="Firefox Pofile" --background-color=000000FF --border-color=E66000FF --border-width=2 --text-color=FFFFFFFF --selection-color=331E54FF --selection-text-color=FFFFFFFF --prompt-color=FFFFFFFF --selection-match-color=E66000FF --dmenu)
+if [ -n "$profile" ]; then
+    firefox -P "$profile"
 fi
