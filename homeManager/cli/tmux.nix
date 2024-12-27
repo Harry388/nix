75ebbee5b543ee386cf609658f04f4012b8bab1d
Harry Thompson <harrythompson3765@gmail.com>
2024-12-27 16:18:29 +0000

    326 tmux colour

--- homeManager/cli/tmux.nix
+++ homeManager/cli/tmux.nix
@@ -19,7 +19,6 @@
         escapeTime = 0;
 
         extraConfig = ''
-            set -ag terminal-overrides ",alacritty:RGB"
             bind -T copy-mode-vi v send -X begin-selection
             bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel
             bind -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel
