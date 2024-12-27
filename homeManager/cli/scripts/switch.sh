89c880f025f69fdd31c78a416c5515989e63b1b0
Harry Thompson <harrythompson3765@gmail.com>
2024-12-27 16:43:14 +0000

    329 switch

--- homeManager/cli/scripts/switch.sh
+++ homeManager/cli/scripts/switch.sh
@@ -4,8 +4,8 @@ sudo nixos-rebuild switch --flake ~/nix
 
 new_generation=$(nixos-rebuild list-generations | grep current | awk '{print $1}')
 
-if [[ $current_generation != $new_generation ]] then
-    git stage *
+if [[ $current_generation != "$new_generation" ]]; then
+    git stage "*"
     git commit -m "$new_generation $1"
     echo "$new_generation: $1"
 else
