bee01237a78d94b50a2d67b27899452cac3a7de0
Harry Thompson <harrythompson3765@gmail.com>
2024-12-27 17:07:09 +0000

    331 session

--- homeManager/cli/scripts/session.sh
+++ homeManager/cli/scripts/session.sh
@@ -3,18 +3,17 @@ dirs=(
     "$HOME/programming/js" "$HOME/programming/zig" "$HOME/programming/rust" "$HOME/programming/gleam" "$HOME/programming/golang" "$HOME/programming/elixir"
 )
 dir=$(find "${dirs[@]}" -maxdepth 1 -mindepth 1 -type d ! -path "*/.*" 2> /dev/null | fzf)
-if [[ -n $dir ]] then
-    name=$(basename $dir)
-    tmux has-session -t $name
-    if [[ $? != 0 ]] then
-        tmux new-session -c $dir -s $name -d
-        if [ -f $dir/.session ]; then
-            tmux send-keys -t $name "source $dir/.session" c-M
+if [ -n "$dir" ]; then
+    name=$(basename "$dir")
+    if ! tmux has-session -t "$name"; then
+        tmux new-session -c "$dir" -s "$name" -d
+        if [ -f "$dir/.session" ]; then
+            tmux send-keys -t "$name" "source $dir/.session" c-M
         fi
     fi
-    if [[ -n $TMUX ]] then
-        tmux switch -t $name
+    if [ -n "$TMUX" ]; then
+        tmux switch -t "$name"
     else
-        tmux attach -t $name
+        tmux attach -t "$name"
     fi
 fi
