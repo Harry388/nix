51bdb5b9399ea9e3e9466e16b20020bebe67d4da
Harry Thompson <harrythompson3765@gmail.com>
2024-12-27 10:43:55 +0000

    134 session name

--- homeManager/cli/zsh.nix
+++ homeManager/cli/zsh.nix
@@ -21,15 +21,16 @@
         '')
         (writeShellScriptBin "session" ''
             dir=$(find ~ ~/work ~/programming ~/programming/js ~/programming/zig ~/programming/rust ~/programming/gleam ~/programming/golang ~/programming/elixir -maxdepth 1 -mindepth 1 -type d ! -path "*/.*" 2> /dev/null | fzf)
+            name=$(basename $dir)
             if [[ -n $dir ]] then
-                tmux has-session -t $dir
+                tmux has-session -t $name
                 if [[ $? != 0 ]] then
-                    tmux new-session -c $dir -s $dir -d
+                    tmux new-session -c $dir -s $name -d
                 fi
                 if [[ -n $TMUX ]] then
-                    tmux switch -t $dir
+                    tmux switch -t $name
                 else
-                    tmux attach -t $dir
+                    tmux attach -t $name
                 fi
             fi
         '')
