b168f1160c04290c04c7d63d3a8549f425756beb
Harry Thompson <harrythompson3765@gmail.com>
2024-12-28 18:19:26 +0000

    335 home test

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -9,6 +9,10 @@
                 "eDP-1, 1920x1200, 0x0, 1"
                 "HDMI-A-1, 1920x1080, 1920x0, 1"
             ];
+
+            home.sessionVariables = {
+                bad = "this should not be set";
+            };
         };
     };
 
