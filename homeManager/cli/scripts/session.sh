5567cd787cac1d5658516a9005fc14e9a31d764a
Harry Thompson <harrythompson3765@gmail.com>
2024-12-28 11:04:46 +0000

    141 session

--- homeManager/cli/scripts/session.sh
+++ homeManager/cli/scripts/session.sh
@@ -1,6 +1,7 @@
 dirs=(
     "$HOME" "$HOME/work" "$HOME/programming"
-    "$HOME/programming/js" "$HOME/programming/zig" "$HOME/programming/rust" "$HOME/programming/gleam" "$HOME/programming/golang" "$HOME/programming/elixir"
+    "$HOME/programming/js" "$HOME/programming/zig" "$HOME/programming/rust"
+    "$HOME/programming/gleam" "$HOME/programming/golang" "$HOME/programming/elixir"
 )
 dir=$(find "${dirs[@]}" -maxdepth 1 -mindepth 1 -type d ! -path "*/.*" 2> /dev/null | fzf)
 if [ -n "$dir" ]; then
@@ -8,7 +9,9 @@ if [ -n "$dir" ]; then
     if ! tmux has-session -t "$name"; then
         tmux new-session -c "$dir" -s "$name" -d
         if [ -f "$dir/.session" ]; then
-            tmux send-keys -t "$name" "source $dir/.session" c-M
+            tmux send-keys -t "$name" "source $dir/.session" c-M clear
+        elif [ -f "$HOME/nix/homeManager/cli/scripts/.session" ]; then
+            tmux send-keys -t "$name" "source $HOME/nix/homeManager/cli/scripts/.session" c-M clear
         fi
     fi
     if [ -n "$TMUX" ]; then
