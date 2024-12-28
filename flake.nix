c733e6039d8a3d5ddee6dc1c3666dfe502ccb90e
Harry Thompson <harrythompson3765@gmail.com>
2024-12-28 12:04:53 +0000

    145 nixpkgs hyprland

--- flake.nix
+++ flake.nix
@@ -14,13 +14,6 @@
             inputs.nixpkgs.follows = "nixpkgs";
         };
 
-        hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-
-        hyprland-plugins = {
-            url = "github:hyprwm/hyprland-plugins";
-            inputs.hyprland.follows = "hyprland";
-        };
-
         ghostty = {
             url = "github:ghostty-org/ghostty";
         };
--- modules/desktops/hyprland.nix
+++ modules/desktops/hyprland.nix
@@ -1,17 +1,9 @@
-{ util, inputs, pkgs, ... }@confInps: util.mkModule { 
+{ util, pkgs, ... }@confInps: util.mkModule {
     inherit confInps;
     name = "hyprland";
 } {
 
-    programs.hyprland = {
-        enable = true;
-        package = inputs.hyprland.packages."${pkgs.system}".hyprland;
-    };
-
-    nix.settings = {
-        substituters = ["https://hyprland.cachix.org"];
-        trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-    };
+    programs.hyprland.enable = true;
 
     services.udisks2.enable = true;
 
