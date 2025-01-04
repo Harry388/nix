fbfcc7a56151f5d5e35b326819e69bd4f6ede485
Harry Thompson <harrythompson3765@gmail.com>
2025-01-04 14:25:55 +0000

    150 nix ghostty config

--- homeManager/cli/ghostty.nix
+++ homeManager/cli/ghostty.nix
@@ -5,6 +5,18 @@
 
     programs.ghostty = {
         enable = true;
+
+        settings = {
+            window-decoration = false;
+
+            background = "181818";
+            background-opacity = 0.9;
+
+            cursor-style = "block";
+            cursor-style-blink = false;
+            shell-integration-features = "no-cursor";
+            cursor-invert-fg-bg = true;
+        };
     };
 
 }
