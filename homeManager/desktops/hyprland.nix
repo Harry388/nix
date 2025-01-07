509ff03e325254f2dbf8ccb410b016a07bc0e3b2
Harry Thompson <harrythompson3765@gmail.com>
2025-01-07 20:47:14 +0000

    342 hypr env

--- homeManager/desktops/hyprland.nix
+++ homeManager/desktops/hyprland.nix
@@ -26,6 +26,8 @@
                 "udiskie"
             ];
 
+            env = "ELECTRON_OZONE_PLATFORM_HINT,auto";
+
             windowrulev2 = [
                 "float, title:(Calculator)"
                 "float, class:(pavucontrol)"
