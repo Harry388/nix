de62f7da83b9c0a7236e72d86ebbe5477f05375f
Harry Thompson <harrythompson3765@gmail.com>
2025-01-28 20:31:15 +0000

    353 nvidia

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -36,7 +36,6 @@
         nvidiaSettings = true;
 
 # Optionally, you may need to select the appropriate driver version for your specific GPU.
-        #package = config.boot.kernelPackages.nvidiaPackages.stable;
         package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
             version = "565.77";
             sha256_64bit = "sha256-CnqnQsRrzzTXZpgkAtF7PbH9s7wbiTRNcM0SPByzFHw=";
