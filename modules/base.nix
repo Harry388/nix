a202f3dbda69ac147f30e6a0fdc667233363f532
Harry Thompson <harrythompson3765@gmail.com>
2025-02-06 12:20:28 +0000

    159 bluetooth

--- modules/base.nix
+++ modules/base.nix
@@ -17,6 +17,8 @@
 
     networking.networkmanager.enable = true;
 
+    hardware.bluetooth.enable = true;
+    hardware.bluetooth.powerOnBoot = true;
     services.blueman.enable = true;
 
     time.timeZone = "Europe/London";
