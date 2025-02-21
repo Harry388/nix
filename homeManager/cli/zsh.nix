eaaf16aa1341fd53de226643b62a2b684e83565d
Harry Thompson <harrythompson3765@gmail.com>
2025-02-21 16:23:20 +0000

    170 switch-env

--- homeManager/cli/zsh.nix
+++ homeManager/cli/zsh.nix
@@ -5,7 +5,7 @@
 
     home.packages = with pkgs; [
         (writeShellScriptBin "switch" (builtins.readFile ./scripts/switch.sh))
-        (writeShellScriptBin "switch-env" (builtins.readFile ./scripts/switch.sh))
+        (writeShellScriptBin "switch-env" (builtins.readFile ./scripts/switch-env.sh))
         (writeShellScriptBin "session" (builtins.readFile ./scripts/session.sh))
         (writeShellScriptBin "firefox-fuzzel" (builtins.readFile ./scripts/firefox-fuzzel.sh))
     ];
