68ef89999f69e0a45cc0a9510b8114be97e33ec3
Harry Thompson <harrythompson3765@gmail.com>
2025-02-21 16:21:21 +0000

    169 switch-env

---
+++ homeManager/cli/scripts/switch-env.sh
@@ -0,0 +1,20 @@
+if [ -z "$XDG_CONFIG_HOME" ]; then
+    XDG_CONFIG_HOME="$HOME/.config"
+fi
+
+if [ -z "$MY_CONFIG" ]; then
+    MY_CONFIG="$HOME/nix/homeManager/config"
+fi
+
+move () {
+    dir="$1"
+    to="$2"
+    rm -rf "$to"
+    cp -R "$dir" "$to"
+}
+
+move_config () {
+    move "$MY_CONFIG/$1" "$XDG_CONFIG_HOME/$1"
+}
+
+move_config "test"
