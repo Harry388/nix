14b9a3d2448ac721b2729e8ec765526e22589fe2
Harry Thompson <harrythompson3765@gmail.com>
2025-02-26 19:49:32 +0000

    378

--- homeManager/nixvim.nix
+++ homeManager/nixvim.nix
@@ -8,7 +8,7 @@
 
         colorschemes.rose-pine = {
             enable = true;
-            settings.variant = "moon";
+            settings.variant = "main";
         };
 
         globals.mapleader = " ";
