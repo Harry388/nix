b832715ee0e9be3ffa8d5b51584f8d12e35e73e7
Harry Thompson <harrythompson3765@gmail.com>
2025-03-11 17:22:27 +0000

    nvim remap and set

--- homeManager/config/nvim/init.lua
+++ homeManager/config/nvim/init.lua
@@ -1,1 +1,1 @@
-print("hi there")
+require("harry")
---
+++ homeManager/config/nvim/lua/harry/remap.lua
@@ -0,0 +1,16 @@
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "-", "<cmd>Yazi<CR>")
+vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<CR>")
+vim.keymap.set("n", "<C-s>", "<cmd>w<CR>")
+vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>")
+vim.keymap.set("n", "<leader>t", "<cmd>tabnew<CR>")
+vim.keymap.set("n", "<M-l>", "<cmd>tabnext<CR>")
+vim.keymap.set("n", "<M-h>", "<cmd>tabprev<CR>")
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprevious<CR>")
+vim.keymap.set("n", "<leader>qq", "<cmd>copen<CR>")
+vim.keymap.set("n", "<leader>c", "\"+")
+vim.keymap.set("n", "<leader>v", "\"_")
+vim.keymap.set("x", "<C-j>", "<cmd>m +1<CR><Esc><S-v>")
+vim.keymap.set("x", "<C-k>", "<cmd>m -2<CR><Esc><S-v>")
