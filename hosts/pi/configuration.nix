12a17040f59d9606ef0c4fcf370b6100e501c548
Harry Thompson <harrythompson3765@gmail.com>
2025-03-24 22:48:02 +0000

    pi autologin

--- hosts/pi/configuration.nix
+++ hosts/pi/configuration.nix
@@ -26,6 +26,8 @@
     defaultApps.enable = false;
     hardware.graphics.enable = lib.mkForce false;
 
+    services.getty.autologinUser = "harry";
+
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
     # on your system were taken. It‘s perfectly fine and recommended to leave
