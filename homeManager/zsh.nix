3af5723627838b9248cf1a1520e5fd8ce0766525
Harry Thompson <harrythompson3765@gmail.com>
2025-04-08 23:47:23 +0100

    465 git aliases

--- homeManager/zsh.nix
+++ homeManager/zsh.nix
@@ -37,7 +37,7 @@
             gf = "git fetch";
             gb = "git branch";
             gk = "git checkout";
-            gl = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'";
+            gl = "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'";
             gla = "gl --all";
         };
 
