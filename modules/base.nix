4efebdbc913a3d46cf516588b37486a6c6dce2e1
Harry Thompson <harrythompson3765@gmail.com>
2025-04-20 11:53:49 +0100

    467 printing

--- modules/base.nix
+++ modules/base.nix
@@ -36,6 +36,12 @@
 
     services.printing.enable = true;
 
+    services.avahi = {
+        enable = true;
+        nssmdns4 = true;
+        openFirewall = true;
+    };
+
     services.pulseaudio.enable = false;
     security.rtkit.enable = true;
     services.pipewire = {
