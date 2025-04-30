5c2cff40b9e0f40fcfe1c94ed3cd271598983d31
Harry Thompson <harrythompson3765@gmail.com>
2025-04-30 11:03:51 +0100

    nvim ctrlf

--- homeManager/config/nvim/lua/config/lazy/telescope.lua
+++ homeManager/config/nvim/lua/config/lazy/telescope.lua
@@ -10,7 +10,7 @@ return {
 
         local telescope = require('telescope.builtin')
 
-        vim.keymap.set("n", "<leader>fg", telescope.git_files)
+        vim.keymap.set("n", "<C-f>", telescope.git_files)
         vim.keymap.set("n", "<leader>ff", telescope.find_files)
         vim.keymap.set('n', '<leader>fs', function()
             telescope.grep_string({ search = vim.fn.input("Grep > ") });
