913a33e9bc77666dcc03dd83fc0c55e20b4aa7a4
Harry Thompson <harrythompson3765@gmail.com>
2025-05-07 19:16:25 +0100

    nvim list modified buffers

--- homeManager/config/nvim/lua/config/remap.lua
+++ homeManager/config/nvim/lua/config/remap.lua
@@ -11,3 +11,23 @@ vim.keymap.set("n", "<leader>c", "\"+")
 vim.keymap.set("n", "<leader>v", "\"_")
 vim.keymap.set("x", "<C-j>", "<cmd>m +1<CR><Esc><S-v>")
 vim.keymap.set("x", "<C-k>", "<cmd>m -2<CR><Esc><S-v>")
+vim.keymap.set("n", "<leader>bm", function ()
+    local bufs = vim.api.nvim_list_bufs()
+    local loaded_modified_bufs = {}
+    local count = 1
+    for _,buf in ipairs(bufs) do
+        if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].modified then
+            full_filename = vim.api.nvim_buf_get_name(buf)
+            filename = vim.fn.fnamemodify(full_filename, ":t")
+            loaded_modified_bufs[count] = {
+                filename = full_filename,
+                text = filename,
+                lnum = 1,
+                col = 1
+            }
+            count = count + 1
+        end
+    end
+    vim.fn.setqflist({}, "r", { title = "Modified Buffers", items = loaded_modified_bufs })
+    vim.cmd("copen")
+end)
