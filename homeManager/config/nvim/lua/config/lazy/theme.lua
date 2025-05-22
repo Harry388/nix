2d93183ac910e8747f95e7e9c2a22e4436b960b5
Harry Thompson <harrythompson3765@gmail.com>
2025-05-23 00:54:50 +0100

    nvim theme

--- homeManager/config/nvim/lua/config/lazy/theme.lua
+++ homeManager/config/nvim/lua/config/lazy/theme.lua
@@ -1,7 +1,18 @@
+-- return {
+-- 	"rose-pine/neovim",
+-- 	name = "rose-pine",
+-- 	config = function()
+-- 		vim.cmd("colorscheme rose-pine")
+-- 	end
+-- }
+
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		vim.cmd("colorscheme rose-pine")
-	end
+  "sainnhe/everforest",
+  config = function()
+      -- " Available values: 'hard', 'medium'(default), 'soft'
+      vim.g.everforest_background = "hard"
+      -- For better performance
+      vim.g.everforest_better_performance = 1
+      vim.cmd("colorscheme everforest")
+  end
 }
