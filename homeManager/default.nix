e3480f007cb9e79016426a003c02c324084a3107
Harry Thompson <harrythompson3765@gmail.com>
2025-05-23 18:27:48 +0100

    small cleanup

--- homeManager/default.nix
+++ homeManager/default.nix
@@ -13,7 +13,6 @@
     defaultHomePkgs.enable = lib.mkDefault true;
     defaultHomeDesktop.enable = lib.mkDefault true;
 
-    programs.google-chrome.enable = lib.mkDefault true;
     syncthing.enable = lib.mkDefault true;
     zsh.enable = lib.mkDefault false;
     switchEnv.enable = lib.mkDefault true;
