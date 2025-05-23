a54b1c8f0f1d38f35a23a10fef9c2c3202e7d930
Harry Thompson <harrythompson3765@gmail.com>
2025-05-23 23:50:01 +0100

    nvim

--- homeManager/config/nvim/lua/config/lazy/lsp.lua
+++ homeManager/config/nvim/lua/config/lazy/lsp.lua
@@ -53,7 +53,7 @@ return {
                     "gopls",
                     "templ",
                     "svelte",
-                    "volar",
+                    "vue_ls",
                     "tailwindcss",
                     "html",
                     -- "htmx",
@@ -125,10 +125,14 @@ return {
         }
 
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts) 
         vim.keymap.set("n", "gD", function() vim.lsp.buf.references() end, opts)
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
         vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
         vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("n", "<leader>fws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
 
         vim.keymap.set("n", "<leader>j", function() vim.diagnostic.goto_next() end, opts)
         vim.keymap.set("n", "<leader>k", function() vim.diagnostic.goto_prev() end, opts)
--- homeManager/config/nvim/lua/config/remap.lua
+++ homeManager/config/nvim/lua/config/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 
+vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set("n", "<C-s>", "<cmd>w<CR>")
 vim.keymap.set("n", "<leader>t", "<cmd>tabnew<CR>")
 vim.keymap.set("n", "<M-l>", "<cmd>tabnext<CR>")
@@ -7,8 +8,8 @@ vim.keymap.set("n", "<M-h>", "<cmd>tabprev<CR>")
 vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
 vim.keymap.set("n", "<M-k>", "<cmd>cprevious<CR>")
 vim.keymap.set("n", "<leader>qq", "<cmd>copen<CR>")
-vim.keymap.set({"n", "v"}, "<leader>c", "\"+")
-vim.keymap.set({"n", "v"}, "<leader>v", "\"_")
+vim.keymap.set({"n", "v"}, "<leader>c", [[\"+]])
+vim.keymap.set({"n", "v"}, "<leader>v", [[\"_]])
 vim.keymap.set("x", "<C-j>", "<cmd>m +1<CR><Esc><S-v>")
 vim.keymap.set("x", "<C-k>", "<cmd>m -2<CR><Esc><S-v>")
 vim.keymap.set("n", "<leader>bm", function ()
