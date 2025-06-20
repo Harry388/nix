30054a3ef9d9c2896d103d8e1d9aea09a313dbd1
Harry Thompson <harrythompson3765@gmail.com>
2025-06-20 23:50:05 +0100

    small nvim theme reorg

--- homeManager/config/nvim/lua/config/lazy/everforest.lua
+++ homeManager/config/nvim/lua/config/lazy/everforest.lua
@@ -1,11 +1,3 @@
--- return {
--- 	"rose-pine/neovim",
--- 	name = "rose-pine",
--- 	config = function()
--- 		vim.cmd("colorscheme rose-pine")
--- 	end
--- }
-
 return {
     "neanias/everforest-nvim",
     version = false,
@@ -15,6 +7,5 @@ return {
         require("everforest").setup({
             background = "hard", -- "hard", "medium", "soft"
         })
-       vim.cmd("colorscheme everforest")
     end
 }
