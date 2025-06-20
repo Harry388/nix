b769ee6b26eb9db02122b6b24ffd71a1f4f4deb6
Harry Thompson <harrythompson3765@gmail.com>
2025-06-20 21:43:53 +0100

    531 syncthing addresses

--- homeManager/syncthing.nix
+++ homeManager/syncthing.nix
@@ -7,9 +7,27 @@
         enable = true;
         settings = {
             devices = {
-                desktop = { id = "LR4PA7T-7SMOYD3-IO5UQZO-3FQQCTB-2X5O4XP-OR5UEG2-32SA5E2-365HZQF"; };
-                laptop = { id = "JL5YEJ2-AEZYV6L-AW3QOB6-OBJ3GHM-DXWPY22-NM635EW-B6QGCC7-3T3G7QL"; };
-                pi = { id = "I2ZV5DI-2XD5J3Y-BAVPKS3-34UTSHH-X3B3ETF-5ND5BFY-QEYRGAN-R4LJCQI"; };
+                desktop = {
+                    id = "LR4PA7T-7SMOYD3-IO5UQZO-3FQQCTB-2X5O4XP-OR5UEG2-32SA5E2-365HZQF";
+                    addresses = [
+                        "tcp://desktop:22000"
+                        "quic://desktop:22000"
+                    ];
+                };
+                laptop = {
+                    id = "JL5YEJ2-AEZYV6L-AW3QOB6-OBJ3GHM-DXWPY22-NM635EW-B6QGCC7-3T3G7QL";
+                    addresses = [
+                        "tcp://laptop:22000"
+                        "quic://laptop:22000"
+                    ];
+                };
+                pi = {
+                    id = "I2ZV5DI-2XD5J3Y-BAVPKS3-34UTSHH-X3B3ETF-5ND5BFY-QEYRGAN-R4LJCQI";
+                    addresses = [
+                        "tcp://pi:22000"
+                        "quic://pi:22000"
+                    ];
+                };
             };
             folders = {
                 documents = {
@@ -28,6 +46,10 @@
                     path = "/home/harry/sync/obsidian";
                     devices = [ "desktop" "laptop" "pi" ];
                 };
+            };
+            options = {
+                localAnnounceEnabled = false;
+                globalAnnounceEnabled = false;
             };
         };
     };
