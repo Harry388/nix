51a4b861860f8ccae18d39da4e28d36f0233efa5
Harry Thompson <harrythompson3765@gmail.com>
2025-07-10 09:35:02 +0100

    waffle session

--- homeManager/sessions/waffle-clfp/shell.fish
+++ homeManager/sessions/waffle-clfp/shell.fish
@@ -1,3 +1,13 @@
+set name "$argv[1]"
+
+if test -z "$SESSIONS_HOME"
+    set SESSIONS_HOME "$HOME/.local/sessions"
+end
+
+if test -d "$SESSIONS_HOME/$name/scripts"
+    fish_add_path -P "$SESSIONS_HOME/$name/scripts"
+end
+
 set TOKEN (grep SUPER_API_TOKEN < .env | cut -d = -f2 | tr -d '"')
 
-alias curl 'curl -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json"'
+alias curl 'curl -s -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json"'
