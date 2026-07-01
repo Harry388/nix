3de4f58ad8e86a76edf41264bea27fc3204f244d
Harry Thompson <harrythompson3765@gmail.com>
2026-07-01 23:03:06 +0100

    added options to icons.nix

--- nix/nixosModules/desktops/icons.nix
+++ nix/nixosModules/desktops/icons.nix
@@ -1,38 +1,64 @@
-inputs: { pkgs, lib, ... }:
+inputs: { pkgs, lib, config, ... }:
 
+let
+    cfg = config.env.icons;
+in
 {
 
-    environment.systemPackages = with pkgs; [
-        apple-cursor
-        banana-cursor
-        papirus-icon-theme
-    ];
+    options.env.icons = {
+        cursor = lib.mkOption {
+            type = lib.types.str;
+            default = "macOS";
+        };
+        cursorSize = lib.mkOption {
+            type = lib.types.int;
+            default = 24;
+        };
+        theme = lib.mkOption {
+            type = lib.types.str;
+            default = "Papirus-Dark";
+        };
+        colourScheme = lib.mkOption {
+            type = lib.types.str;
+            default = "prefer-dark";
+        };
+    };
 
-    xdg.icons.fallbackCursorThemes = [
-        "macOS"
-    ];
+    config = {
 
-    # NOTE: icon theme is also set in:
-    # environment/config/gtk-3.0/settings.ini
-    # environment/config/gtk-4.0/settings.ini
-    # environment/config/vicinae/settings.json
+        environment.systemPackages = with pkgs; [
+            apple-cursor
+            banana-cursor
+            papirus-icon-theme
+        ];
 
-    programs.dconf.profiles.user.databases = [
-        {
-            settings = {
-                "org/gnome/desktop/interface" = {
-                    icon-theme = "Papirus-Dark";
-                    color-scheme = "prefer-dark";
-                    cursor-theme = "macOS";
-                    cursor-size = lib.gvariant.mkInt32 24;
+        xdg.icons.fallbackCursorThemes = [
+            cfg.cursor
+        ];
+
+        # NOTE: icon theme is also set in:
+        # environment/config/gtk-3.0/settings.ini
+        # environment/config/gtk-4.0/settings.ini
+        # environment/config/vicinae/settings.json
+
+        programs.dconf.profiles.user.databases = [
+            {
+                settings = {
+                    "org/gnome/desktop/interface" = {
+                        icon-theme = cfg.theme;
+                        color-scheme = cfg.colourScheme;
+                        cursor-theme = cfg.cursor;
+                        cursor-size = lib.gvariant.mkInt32 cfg.cursorSize;
+                    };
                 };
-            };
-        }
-    ];
+            }
+        ];
 
-    environment.variables = {
-        XCURSOR_THEME = "macOS";
-        XCURSOR_SIZE = "24";
+        environment.variables = {
+            XCURSOR_THEME = cfg.cursor;
+            XCURSOR_SIZE = cfg.cursorSize;
+        };
+
     };
 
 }
