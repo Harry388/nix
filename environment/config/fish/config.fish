3c8424f0aedfb82fd464948bef6ad7fbfebd9d47
Harry Thompson <harrythompson3765@gmail.com>
2026-07-07 16:24:15 +0100

    Revert "removed auto commit"
    This reverts commit 0a5552363962d6dddb8367dea574e20778d93fab.

--- environment/config/fish/config.fish
+++ environment/config/fish/config.fish
@@ -25,6 +25,7 @@ bind ctrl-space accept-autosuggestion
 alias clear "clear -x"
 alias sail "sh $([ -f sail ] && echo sail || echo vendor/bin/sail)"
 alias git-nuke "git reset --hard HEAD && git clean -fd"
+alias git-auto-commit "git add -A && git commit -m"
 alias git-log-pretty "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'"
 
 if test "$TERM" = dumb
