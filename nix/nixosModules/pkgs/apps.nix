070873326d97c8a3ea0af1c1d51bc980f174c467
Harry Thompson <harrythompson3765@gmail.com>
2026-08-27 17:23:33 +0100

    update

--- nix/nixosModules/pkgs/apps.nix
+++ nix/nixosModules/pkgs/apps.nix
@@ -9,15 +9,6 @@ in
         vial
     ];
 
-    # NOTE: this is a temporary fix for openldap, which is a dependency of bottles
-    nixpkgs.overlays = [
-        (_: prev: {
-            openldap = prev.openldap.overrideAttrs {
-                doCheck = !prev.stdenv.hostPlatform.isi686;
-            };
-        })
-    ];
-
     environment.systemPackages = with pkgs; [
         alacritty
         sqlitebrowser
